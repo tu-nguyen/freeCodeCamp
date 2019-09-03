9df97812b317184d1528197ff98e23004cd7cd3a
tu-nguyen <valcrune@gmail.com>
2019-09-03 16:00:56 -0400

    wrapped up finishing touches to project

--- rockpaperscissor/app.js
+++ rockpaperscissor/app.js
@@ -21,28 +21,37 @@ function convertToWord(letter) {
 }
 
 function win(userChoice, computerChoice) {
+    const smallUserWord = "user".fontsize(3).sub();
+    const smallCompWord = "comp".fontsize(3).sub();
+    const userChoice_div = document.getElementById(userChoice);
     userScore++;
     userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML = computerScore;
-    const smallUserWord = "user".fontsize(3).sub();
-    const smallCompWord = "comp".fontsize(3).sub();
     result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} beats ${convertToWord(computerChoice)}${smallCompWord}. You win!  `;
+    userChoice_div.classList.add("green-glow")
+    setTimeout(() => userChoice_div.classList.remove("green-glow"), 300)
 }
 
+
 function lose(userChoice, computerChoice) {
+    const smallUserWord = "user".fontsize(3).sub();
+    const smallCompWord = "comp".fontsize(3).sub();
+    const userChoice_div = document.getElementById(userChoice);
     computerScore++;
     userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML = computerScore;
-    const smallUserWord = "user".fontsize(3).sub();
-    const smallCompWord = "comp".fontsize(3).sub();
-    result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} beats ${convertToWord(computerChoice)}${smallCompWord}. You lose...`;
+    result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} loses to ${convertToWord(computerChoice)}${smallCompWord}. You lose...`;
+    userChoice_div.classList.add("red-glow");
+    setTimeout(() => userChoice_div.classList.remove("red-glow"), 300);
 }
 
 function draw(userChoice, computerChoice) {
-
     const smallUserWord = "user".fontsize(3).sub();
     const smallCompWord = "comp".fontsize(3).sub();
+    const userChoice_div = document.getElementById(userChoice);
     result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} equals ${convertToWord(computerChoice)}${smallCompWord}. It's a draw.`;
+    userChoice_div.classList.add("gray-glow")
+    setTimeout(() => userChoice_div.classList.remove("gray-glow"), 300)
 }
 
 function game(userChoice) {
@@ -67,17 +76,11 @@ function game(userChoice) {
 }
 
 function main() {
-    rock_div.addEventListener("click", function () {
-        game("r");
-    })
+    rock_div.addEventListener("click", () => game("r"));
 
-    paper_div.addEventListener("click", function () {
-        game("p")
-    })
+    paper_div.addEventListener("click", () => game("p"));
 
-    scissors_div.addEventListener("click", function () {
-        game("s")
-    })
+    scissors_div.addEventListener("click", () => game("s"));
 }
 
 main();
